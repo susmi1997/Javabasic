99eef606701060b39db2820221b043fb0fada161
Susmita Mukherjee <98667361+susmi1997@users.noreply.github.com>
2023-01-11 20:25:39 +0530

    Create PrintMain1.java

---
+++ PrintMain1.java
@@ -0,0 +1,9 @@
+package main;
+
+public class PrintMain1 {
+public static void main(String[] args) {
+	System.out.print("Susmita Mukherjee,\t");
+	System.out.print("who is learning JAVA\t");
+	System.out.println("from India");
+}
+}
