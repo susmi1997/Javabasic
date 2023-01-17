f71d31d84c27587f9236d6bb341ab91d212d6b6a
Susmita Mukherjee <98667361+susmi1997@users.noreply.github.com>
2023-01-17 21:25:24 +0530

    Create VariableMain.java

---
+++ VariableMain.java
@@ -0,0 +1,10 @@
+package main;
+
+public class VariableMain {
+   public static void main (String[]args) {
+	   int myVar;
+	   myVar=800;
+	   myVar= myVar+10;
+	   System.out.println("Value Of MY Variable Is="+myVar);
+   }
+}
