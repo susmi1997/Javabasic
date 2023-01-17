b234c1ed70f93311b44e5dc3c3f6f8be9087da04
Susmita Mukherjee <98667361+susmi1997@users.noreply.github.com>
2023-01-17 21:44:03 +0530

    Create VariableMain2.java

---
+++ VariableMain2.java
@@ -0,0 +1,14 @@
+package main;
+
+public class VariableMain2 {
+
+	public static void main(String[] args) {
+//Students Stipends	depends on their CGPA Of x
+       int x=10000;
+       System.out.println("Stipend of student 1"+(3.47*x));
+       System.out.println("Stipend of student 2"+(3.65*x));
+       System.out.println("Stipend of student 3"+(3.25*x));
+       System.out.println("Stipend of student 4"+(3.98*x));
+	}
+
+}
