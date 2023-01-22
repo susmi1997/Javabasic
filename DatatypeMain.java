7f41726c3278c9289753975608d4c222ef4548c8
Susmita Mukherjee <98667361+susmi1997@users.noreply.github.com>
2023-01-22 22:04:02 +0530

    Create DatatypeMain.java

---
+++ DatatypeMain.java
@@ -0,0 +1,15 @@
+package main;
+
+public class DatatypeMain {
+
+	public static void main(String[] args) {
+		int x,y,z;
+		x=10;
+		y= -7;
+		z= 20;
+		float avg;
+		avg= (float)(x+y+z)/3;
+System.out.println("Average of x,y,z is:"+avg);
+	}
+
+}
