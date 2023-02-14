f2226e5c47f19a1133388c88023c91b49bae11bf
Susmita Mukherjee <98667361+susmi1997@users.noreply.github.com>
2023-02-14 21:18:50 +0530

    Create TrueFalseMain.java

---
+++ TrueFalseMain.java
@@ -0,0 +1,15 @@
+package main;
+
+public class TrueFalseMain {
+
+	public static void main(String[] args) {
+		boolean Sus;
+		boolean Susmita;
+		Sus= false;
+		Susmita= true;
+		System.out.println("When Check MY Name Is Sus-" +Sus);
+		System.out.println("When Check MY Name Is Susmita-" +Susmita);
+
+	}
+
+}
