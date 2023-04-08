33d4308ddee4340dcd7c2b0d5454579f8e2b824c
Susmita Mukherjee <98667361+susmi1997@users.noreply.github.com>
2023-04-08 21:40:37 +0530

    Create IfelseCondition.java

---
+++ IfelseCondition.java
@@ -0,0 +1,19 @@
+package main;
+//when get output
+public class Ifelsecondition {
+public static void main(String[]args) {
+	int x=20;
+	if(x<400) {
+	System.out.println("Susmita"+x);
+}
+}
+
+
+///when no output
+
+int y=15;{
+if(y>500) {
+	System.out.println("Sus"+y);
+}
+}
+}
