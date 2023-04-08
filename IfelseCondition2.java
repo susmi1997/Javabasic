4adec198861597f1b1edca6633b15ed6604f2dae
Susmita Mukherjee <98667361+susmi1997@users.noreply.github.com>
2023-04-08 22:06:25 +0530

    Create IfelseCondition2.java

---
+++ IfelseCondition2.java
@@ -0,0 +1,22 @@
+package main;
+public class IfelseCondition2 {
+public static void main (String[]args) {
+	//int x=22;
+	//if (x%2== 0 && x%5== 0) {     //when value use &&
+		//System.out.println("hello");//here not-printed output due to value of x=22, its should be 20 if we wanted output
+//	}
+//}
+//} 
+	
+	
+	
+	
+//when value use ||
+	
+	
+int y=22;
+if (y%2== 0 || y%5== 0) {
+	System.out.println("hi sus"); //here printed output
+}
+}
+} 
